3f2d9d6b2de416b295e3f3aa0a5a2e6c9b33d433
Adam <50333588+adamcpenman@users.noreply.github.com>
2019-10-21 18:45:48 -0500

    redux added
    redux implemented in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import "./styles.css";
 import React from "react";
 import ReactDOM from "react-dom";
+//importing redux
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { reducer } from "./reducers/title";
 import App from "./components/App";
 
+//global JS object
+const store = createStore(reducer);
+
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
